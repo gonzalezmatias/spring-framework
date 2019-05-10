feec0a5b7af791ed0a5cc82f2cc7d3523639edbd
Matias E Gonzalez <mgonzalez@temperies.com>
2019-05-10 13:05:59 -0300

    fix

--- src/main/java/com/example/tutorial/mail/MailConfig.java
+++ src/main/java/com/example/tutorial/mail/MailConfig.java
@@ -1,5 +1,16 @@
 package com.example.tutorial.mail;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration 
 public class MailConfig {
 
+	public MailSender mockMailSender() {
+		return new MockMailSender();
+	}
+	
+
+	public MailSender smtpMailSender() {
+		return new SmtpMailSender();
+	}
 }
